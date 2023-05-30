fe04832978b93f36b6d3b7eccf850d86bf8725a8
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-05-30 18:48:08 +0530

    Updated Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,29 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword!=this.password) return;
+        if(newPasdword.length()<8) return;
+       // boolean cha=false;
+        boolean uppercase=false;
+        boolean lowercase=false;
+        boolean digit=false;
+        boolean schar=false;
+        for(int i=0;i<newPassword.length();i++)
+        {
+            if(newPassword.charAt(i)>=33 && newPassword.charAt(i)<=47 || newPassword.charAt(i)>=58 && newPassword.charAt(i)<=64 || 
+              newPassword.charAt(i)>=91 && newPassword.charAt(i)<=96 || newPassword.charAt(i)>=123 && newPassword.charAt(i)<=126)
+            {
+                schar=true;
+            }
+            if(newPassword.charAt(i)>=65 && newPassword.charAt(i)<=90) uppercase=true;
+            if(newPassword.charAt(i)>=97 && newPassword.charAt(i)<=122) lowercase=true;
+            if(newPassword.charAt(i)>=48 && newPassword.charAt(i)<=57) digit=true;
+        }
+        if(schar && uppercase && lowercase && digit) 
+        {
+            this.password=newPassword;
+        }
+            
+        
     }
 }
